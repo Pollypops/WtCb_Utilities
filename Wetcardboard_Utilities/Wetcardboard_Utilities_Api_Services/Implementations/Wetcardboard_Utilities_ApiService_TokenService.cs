d09504a8fdc3384283041fe22d81f8754c3ab790
Pollypops <103785024+Pollypops@users.noreply.github.com>
2023-04-16 15:58:05 +0200

    Add err logging to TokenService

--- Wetcardboard_Utilities/Wetcardboard_Utilities_Api_Services/Implementations/Wetcardboard_Utilities_ApiService_TokenService.cs
+++ Wetcardboard_Utilities/Wetcardboard_Utilities_Api_Services/Implementations/Wetcardboard_Utilities_ApiService_TokenService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Wetcardboard_Shared.Logging;
 using Wetcardboard_Utilities_Api_Services.Interfaces;
 using Wetcardboard_Utilities_Models.Front_End;
 
@@ -11,6 +12,7 @@ namespace Wetcardboard_Utilities_Api_Services.Implementations
         #region Fields & Properties
         #region Fields
         private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IWtCbLogger _logger;
 
         private const string _apiTokenPath = "Token";
         #endregion \ Fields
@@ -30,10 +32,12 @@ namespace Wetcardboard_Utilities_Api_Services.Implementations
         #region Constructor
         public Wetcardboard_Utilities_ApiService_TokenService(
                 IHttpClientFactory httpClientFactory,
+                IWtCbLogger logger,
                 Wetcardboard_Utilities_Fe_Appsettings appSettings
             ) : base(appSettings)
         {
             _httpClientFactory = httpClientFactory;
+            _logger = logger;
         }
         #endregion \ Constructor
 
@@ -50,6 +54,8 @@ namespace Wetcardboard_Utilities_Api_Services.Implementations
             var respMsg = await http.SendAsync(reqMsg);
             if (respMsg.StatusCode != System.Net.HttpStatusCode.OK)
             {
+                var logMsg = $"Error creating JwtToken - Backend request responded with error code: {respMsg.StatusCode}, response message: {respMsg.RequestMessage}";
+                _logger.Log(logMsg, LogLevel.Error);
                 return false;
             }
             return true;
