082ae7656f7443ba33232cdb1aa08dda307437f8
Pollypops <103785024+Pollypops@users.noreply.github.com>
2023-05-23 19:42:26 +0200

    Add exception extension class
    Add exception extension class

---
+++ Wetcardboard_Utilities/Wetcardboard_Utilities_General/Extensions/Extension_Exception.cs
@@ -0,0 +1,19 @@
+﻿namespace Wetcardboard_General.Extensions
+{
+    public static class Extension_Exception
+    {
+        public static string GetFullExceptionMessage(this Exception ex)
+        {
+            var res = string.Empty;
+            if (ex != null)
+            {
+                res = $"{ex.Message}{Environment.NewLine}{Environment.NewLine}";
+                if (ex.InnerException != null)
+                {
+                    res += GetFullExceptionMessage(ex.InnerException);
+                }
+            }
+            return res;
+        }
+    }
+}
